ae83da6febef2b26cb188769a9fa2e247d0fa9f9
AndrezaAmorim <amrimandreza@gmail.com>
2020-05-18 18:16:21 -0300

    chore: reorganização dos components com a shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExerContadorComponent } from './exer-contador/exer-contador.component';
-import { ExerDataBindingComponent } from './exer-data-binding/exer-data-binding.component';
-import { ExerDiretivasComponent } from './exer-diretivas/exer-diretivas.component';
-import { ExerNgclassComponent } from './exer-ngclass/exer-ngclass.component';
-import { ExerPipesComponent } from './exer-pipes/exer-pipes.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ExerContadorComponent } from './share/components/exer-contador/exer-contador.component';
+import { ExerDataBindingComponent } from './share/components/exer-data-binding/exer-data-binding.component';
+import { ExerDiretivasComponent } from './share/components/exer-diretivas/exer-diretivas.component';
+import { ExerNgclassComponent } from './share/components/exer-ngclass/exer-ngclass.component';
+import { ExerPipesComponent } from './share/components/exer-pipes/exer-pipes.component';
+import { FooterComponent } from './share/components/footer/footer.component';
+import { HeaderComponent } from './share/components/header/header.component';
 
 registerLocaleData(localePt, 'pt');
 
