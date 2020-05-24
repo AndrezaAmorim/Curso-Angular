d6725fbcc192fd336288ca05192d6fb86d01945d
AndrezaAmorim <amrimandreza@gmail.com>
2020-05-24 15:03:08 -0300

    feat: adicionado guardas de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,23 +7,30 @@ import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { EstaLogadoGuard } from './share/guards/esta-logado/esta-logado.guard';
+import { NaoEstaLogadoGuard } from './share/guards/nao-esta-logado/nao-esta-logado.guard';
 
 
 const routes: Routes = [{
   path: 'home',
   component: HomeComponent,
+  canActivate: [EstaLogadoGuard]
 }, {
   path: 'login',
   component: LoginComponent,
+  canActivate: [NaoEstaLogadoGuard]
 }, {
   path: 'extrato',
   component: ExtratoComponent,
+  canActivate: [EstaLogadoGuard]
 }, {
   path: 'contatos',
   component: ContatosComponent,
+  canActivate: [EstaLogadoGuard]
 }, {
   path: 'contatos/:id',
   component: DetalhesContatoComponent,
+  canActivate: [EstaLogadoGuard]
 }, {
   path: '',
   redirectTo: 'home',
